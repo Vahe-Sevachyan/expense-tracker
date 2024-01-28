97916a82ea3e18e718e56b8fde0d7f489af21c05
Vahe Sevachyan <vscode13@gmail.com>
2024-01-27 20:37:04 -0800

    prepared component layout boiler plate

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,26 @@
-<template>Hello World!</template>
+<template>
+  <HeaderC />
+  <div class="container">
+    <BalanceC />
+    <IncomeExpenses />
+    <TransactionList />
+    <AddTransaction />
+  </div>
+</template>
+
+<script>
+import HeaderC from './components/HeaderC.vue'
+import BalanceC from './components/BalanceC.vue'
+import IncomeExpenses from './components/IncomeExpenses.vue'
+import TransactionList from './components/TransactionList.vue'
+import AddTransaction from './components/AddTransaction.vue'
+export default {
+  components: {
+    HeaderC,
+    BalanceC,
+    IncomeExpenses,
+    TransactionList,
+    AddTransaction
+  }
+}
+</script>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,7 @@
+<template>
+  <h3>History</h3>
+  <ul id="list" class="list">
+    <li class="minus">Laptop <span>$400</span><button class="delete-btn">X</button></li>
+    <li class="plus">PayCheck <span>$20,000</span><button class="delete-btn">X</button></li>
+  </ul>
+</template>
