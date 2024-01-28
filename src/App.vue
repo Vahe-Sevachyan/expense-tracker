7bf6f3350b99ec1ac63e5f5ddc23cf769314c1e9
Vahe Sevachyan <vscode13@gmail.com>
2024-01-28 13:40:49 -0800

    updated transaction list component

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,22 @@
   <div class="container">
     <BalanceC />
     <IncomeExpenses />
-    <TransactionList />
+    <TransactionList v-bind:transactions="transactions" />
     <AddTransaction />
   </div>
 </template>
 
-<script>
+<script setup>
 import HeaderC from './components/HeaderC.vue'
 import BalanceC from './components/BalanceC.vue'
 import IncomeExpenses from './components/IncomeExpenses.vue'
 import TransactionList from './components/TransactionList.vue'
 import AddTransaction from './components/AddTransaction.vue'
-export default {
-  components: {
-    HeaderC,
-    BalanceC,
-    IncomeExpenses,
-    TransactionList,
-    AddTransaction
-  }
-}
+import { ref } from 'vue'
+const transactions = ref([
+  { id: 1, text: 'Headphones', amount: -119.99 },
+  { id: 1, text: 'Books', amount: -89.99 },
+  { id: 1, text: 'Salary', amount: 8930.84 },
+  { id: 1, text: 'laptop', amount: -419.99 }
+])
 </script>
--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,7 +1,24 @@
 <template>
   <h3>History</h3>
   <ul id="list" class="list">
-    <li class="minus">Laptop <span>$400</span><button class="delete-btn">X</button></li>
-    <li class="plus">PayCheck <span>$20,000</span><button class="delete-btn">X</button></li>
+    <li
+      v-for="transaction in transactions"
+      v-bind:key="transaction.id"
+      v-bind:class="transaction.amount < 0 ? 'minus' : 'plus'"
+    >
+      {{ transaction.text }} <span>${{ transaction.amount }}</span
+      ><button class="delete-btn">X</button>
+    </li>
+    <!-- <li class="minus">Laptop <span>$400</span><button class="delete-btn">X</button></li>
+    <li class="plus">PayCheck <span>$20,000</span><button class="delete-btn">X</button></li> -->
   </ul>
 </template>
+<script setup>
+import { defineProps } from 'vue'
+const props = defineProps({
+  transactions: {
+    type: Array,
+    required: true
+  }
+})
+</script>
