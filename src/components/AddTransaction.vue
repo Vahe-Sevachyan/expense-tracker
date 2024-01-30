a3f6e054fd971fbb3dad5910892fd37bbb50528f
Vahe Sevachyan <vscode13@gmail.com>
2024-01-29 19:56:14 -0800

    updated toastification boiler plate

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -16,7 +16,17 @@
 </template>
 <script setup>
 import { ref } from 'vue';
+import { useToast } from 'vue-toastification';
+const text = ref('');
+const amount = ref('');
+const emit = defineEmits([transactionSubmitted]);
+const toast = useToast();
 const onSubmit = () => {
-  console.log('clicked');
+  if (!text.value || !amount.value) {
+    toast.error('Both Fields must be filled');
+    return;
+  }
+  text.value = '';
+  amount.value = '';
 };
 </script>
